9014b8a930d3ae68193eaf9d3d9936fde587e0d2
Thibaut <108188070+ThibautSnoeijs@users.noreply.github.com>
2022-06-28 15:40:48 +0000

    Embraced bashisms
    Original already wasn't POSIX compliant anyways

--- FizzBuzz.sh
+++ FizzBuzz.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 n=0
 while true;do
-	n=$((n+1))
-	x=""
-	if [ $(( n % 3 )) -eq 0 ];then
-		x+=Fizz
-	fi
-	if [ $(( n % 5 )) -eq 0 ];then
-		x+=Buzz
-	fi
-	if [ -z $x ];then
-		echo $n
-	else
-		echo $x
-	fi
+	n=$((n+1));x=""
+	[[ $(( n % 3 )) -eq 0 ]]&&x+=Fizz
+	[[ $(( n % 5 )) -eq 0 ]]&&x+=Buzz
+	[[ -z $x ]]&&x=$n
+	echo $x
 done
