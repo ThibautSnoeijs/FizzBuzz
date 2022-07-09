d98bf8d6d6564660ff9a9210c76d10482b3b2a0f
Thibaut <108188070+ThibautSnoeijs@users.noreply.github.com>
2022-07-09 13:51:52 +0000

    Compacted Java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,8 +1,7 @@
-public class FizzBuzz {
-	public static void main(String[] args) {
-		int n=1;String x;
-		for(;;){n+=1;x="";
-			if(n%3==0)x+="Fizz";
-			if(n%5==0)x+="Buzz";
-			if(x=="")System.out.print(n);
-			System.out.println(x);}}}
+public class FizzBuzz{public static void main(String[] args){
+int n=1;
+for(;;){n+=1;String x="";
+	if(n%3==0)x+="Fizz";
+	if(n%5==0)x+="Buzz";
+	if(x=="")System.out.print(n);
+	System.out.println(x);}}}
