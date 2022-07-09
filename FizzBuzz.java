314f329c7d46d5a5d7b57ede3544eaff320292a7
Thibaut <108188070+ThibautSnoeijs@users.noreply.github.com>
2022-07-09 11:18:02 +0000

    replaced int with boolean where possible

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,8 +1,8 @@
 public class FizzBuzz {
 	public static void main(String[] args) {
-		int n=1;int x=0;
-		for(;;){n+=1;x=0;
-			if(n%3==0){x=1;System.out.print("Fizz");}
-			if(n%5==0){x=1;System.out.print("Buzz");}
-			if(x==0)System.out.print(n);
+		int n=1;boolean x;
+		for(;;){n+=1;x=true;
+			if(n%3==0){x=false;System.out.print("Fizz");}
+			if(n%5==0){x=false;System.out.print("Buzz");}
+			if(x)System.out.print(n);
 			System.out.print("\n");}}}
