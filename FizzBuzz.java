628be099412ac565bd281b2f01761f353ff1039f
Thibaut <108188070+ThibautSnoeijs@users.noreply.github.com>
2022-07-09 11:51:18 +0000

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,8 +1,8 @@
 public class FizzBuzz {
 	public static void main(String[] args) {
-		int n=1;boolean x;
-		for(;;){n+=1;x=true;
-			if(n%3==0){x=false;System.out.print("Fizz");}
-			if(n%5==0){x=false;System.out.print("Buzz");}
-			if(x)System.out.print(n);
-			System.out.print("\n");}}}
+		int n=1;String x;
+		for(;;){n+=1;x="";
+			if(n%3==0)x+="Fizz";
+			if(n%5==0)x+="Buzz";
+			if(x=="")System.out.print(n);
+			System.out.println(x);}}}
