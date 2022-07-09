02b090544e2afc9634d02afed19b641fa697f09b
Thibaut <108188070+ThibautSnoeijs@users.noreply.github.com>
2022-07-09 13:51:20 +0000

    Compacted pascal

--- FizzBuzz.pas
+++ FizzBuzz.pas
@@ -1,15 +1,16 @@
 program FizzBuzz;
 var
-n: integer;
-x: boolean;
+i:integer;
+x:string;
 begin
-n:=1;
-while n>0 do
-	begin
-	x:=true;
-	if n mod 3 = 0 then begin x:=false;write('Fizz') end;
-	if n mod 5 = 0 then begin x:=false;write('Buzz') end;
-	if x then write(n);
-	writeln();n+=1;
+i:=1;
+while i>0 do
+	begin;
+	x:='';
+	if i mod 3 = 0 then x+='Fizz';
+	if i mod 5 = 0 then x+='Buzz';
+	if x='' then write(i);
+	writeln(x);
+	i+=1;
 	end;
 end.
