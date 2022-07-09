995872a793142982726f2849d8a011d0fcd6e8f6
Thibaut <108188070+ThibautSnoeijs@users.noreply.github.com>
2022-07-09 12:21:01 +0000

    Compacted bash

--- FizzBuzz.bash
+++ FizzBuzz.bash
@@ -1,9 +1,7 @@
-#!/bin/bash
 n=0
-while true;do
-	n=$((n+1));x=""
-	[[ $(( n % 3 )) -eq 0 ]]&&x+=Fizz
-	[[ $(( n % 5 )) -eq 0 ]]&&x+=Buzz
+while true;do((n++));x=""
+	(( $n % 3 ))||x+=Fizz
+	(( $n % 5 ))||x+=Buzz
 	[[ -z $x ]]&&x=$n
 	echo $x
 done
