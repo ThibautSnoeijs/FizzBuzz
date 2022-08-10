456a31705bd9d671817b71ef18d6b7c2e8fca0d0
Thibaut <108188070+ThibautSnoeijs@users.noreply.github.com>
2022-08-10 15:45:33 +0000

    Compacted and renamed JS

--- FizzBuzz.js
+++
@@ -1,6 +0,0 @@
-n=0;
-for(;;){n+=1;x="";
-	if(n%3==0)x+="Fizz";
-    if(n%5==0)x+="Buzz";
-	if(x=="")x=n;console.log(x)
-}
---
+++ JavaScript.js
@@ -0,0 +1,1 @@
+for(n=0;;n+=1)x="",n%3==0&&(x+="Fizz"),n%5==0&&(x+="Buzz"),""==x&&(x=n),console.log(x);
