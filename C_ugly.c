30495762da679ea97f2c83ac23c61e0cf6957ae7
Thibaut <108188070+ThibautSnoeijs@users.noreply.github.com>
2023-02-04 11:33:59 +0100

    Create C_ugly.c

---
+++ C_ugly.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int
+	first_n	=1,
+	last_n	=12345678;
+
+int n,x,f,b;
+
+int main(){
+	n=first_n;
+	f=n%3;
+	b=b%5;
+	for(;n<=last_n;n++){
+		x=(b==5);
+		x<<=1;
+		x+=(f==3);
+		switch (x){
+			case 0:printf("%d\n",n);break;
+			case 1:puts("Fizz");f=0;break;
+			case 2:puts("Buzz");f=0;break;
+			case 3:puts("FizzBuzz");f,b=0,0;break;
+			default:__builtin_unreachable();
+			}
+		}
+	}
